c406896427f09733d40fc6c5a7e3e05082460af4
inaciose <inaciose@gmail.com>
2020-02-20 13:40:32 +0000

    team def

--- player_sinacio/script/player_node.py
+++ player_sinacio/script/player_node.py
@@ -1,7 +1,64 @@
 #!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+
+class Player:
+    def __init__(self, player_name):
+        self.player_name = player_name
+        rospy.loginfo("I am " + self.player_name)
+
+        red_team = rospy.get_param('/red_team')
+        green_team = rospy.get_param('/green_team')
+        blue_team = rospy.get_param('/blue_team')
+
+        print("red_team = " + str(red_team))
+        print("green_team = " + str(green_team))
+        print("blue_team = " + str(blue_team))
+
+        if self.player_name in red_team:
+            self.my_team, self.prey_team, self.hunter_team = 'red', 'green', 'blue'
+            self.my_players, self.preys, self.hunters = red_team, green_team, blue_team
+
+        elif self.player_name in green_team:
+            self.my_team, self.prey_team, self.hunter_team = 'green', 'blue', 'red'
+            self.my_players, self.preys, self.hunters = green_team, blue_team, red_team
+
+        elif self.player_name in blue_team:
+            self.my_team, self.prey_team, self.hunter_team = 'blue', 'green', 'red'
+            self.my_players, self.preys, self.hunters = blue_team, green_team, red_team
+
+        #if self.player_name in red_team:
+        #    self.my_team = 'red'
+        #    self.prey_team = 'green'
+        #    self.hunter_team = 'blue'
+        #elif self.player_name in green_team:
+        #    self.my_team = 'green'
+        #    self.prey_team = 'blue'
+        #    self.hunter_team = 'red'
+        #elif self.player_name in blue_team:
+        #    self.my_team = 'blue'
+        #    self.prey_team = 'red'
+        #    self.hunter_team = 'green'
+        
+        else:
+            rospy.logerr("My name is not in any team list")
+            exit(0)
+
+        rospy.loginfo("I am " + self.player_name + " and im in the " + self.my_team + " hunting " + self.prey_team + " run away from " + self.hunter_team)
+        
+def callback(msg):
+    #rospy.loginfo(rospy.get_caller_id() + "I heard %s", msg.data)
+    print("recieved a message containing string " + msg.data)
+
 
 def main():
     print("hello world node")
+    rospy.init_node('sinacio', anonymous=False)
+    player = Player("sinacio")
+
+    rospy.Subscriber("chatter", String, callback)
+
+    rospy.spin()
 
 if __name__ == "__main__":
     main()
